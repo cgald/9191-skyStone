dcc449bca96c6d7c5860526d8405ae71d01293f9
cgald <chase_gould@northlandchristian.org>
2019-11-07 13:54:28 -0600

    Reverting change, didn't work

--- SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RevTele.java
+++ SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RevTele.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "RevTele", group = "9191")
 public class RevTele extends OpMode {
