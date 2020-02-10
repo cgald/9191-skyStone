83ce9de4f41bbe2bd1d6b409a6b7ca3908ea5e34
cgald <chase_gould@northlandchristian.org>
2020-02-10 15:14:02 -0600

    things

--- SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/mixedTele.java
+++ SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/mixedTele.java
@@ -1,40 +1,38 @@
 package org.firstinspires.ftc.robotcontroller.internal;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "mixTele", group = "9191")
 public class mixedTele extends OpMode {
-    private DcMotor frontLeft;
+    /*private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
-    private DcMotor linearLift;
+    private DcMotor linearLift;*/
     private DcMotor intakeLeft;
     private DcMotor intakeRight;
-    private CRServo gripperLeft;
+    /*private CRServo gripperLeft;
     private CRServo gripperRight;
     private Servo foundationOne;
     private Servo foundationTwo;
     private Servo capStone;
-    private CRServo pushBlock;
+    private CRServo pushBlock;*/
     @Override
     public void init() {
-        frontLeft = hardwareMap.dcMotor.get("FL");
+        /*frontLeft = hardwareMap.dcMotor.get("FL");
         frontRight = hardwareMap.dcMotor.get("FR");
         backLeft = hardwareMap.dcMotor.get("BL");
         backRight = hardwareMap.dcMotor.get("BR");
-        linearLift = hardwareMap.dcMotor.get("LL");
+        linearLift = hardwareMap.dcMotor.get("LL");*/
         intakeLeft = hardwareMap.dcMotor.get("IL");
         intakeRight = hardwareMap.dcMotor.get("IR");
-        gripperLeft = hardwareMap.crservo.get("GF");
+        /*gripperLeft = hardwareMap.crservo.get("GF");
         gripperRight = hardwareMap.crservo.get("GB");
         foundationOne = hardwareMap.servo.get("F1");
         foundationTwo = hardwareMap.servo.get("F2");
         pushBlock = hardwareMap.crservo.get("PB");
-        capStone = hardwareMap.servo.get("CS");
+        capStone = hardwareMap.servo.get("CS");*/
     }
 
     @Override
@@ -46,10 +44,10 @@ public class mixedTele extends OpMode {
         final double v2 = r * Math.sin(robotAngle) - rightX;
         final double v3 = r * Math.sin(robotAngle) + rightX;
         final double v4 = r * Math.cos(robotAngle) - rightX;
-        frontLeft.setPower(v1);
+        /*frontLeft.setPower(v1);
         frontRight.setPower(v2);
         backLeft.setPower(v3);
-        backRight.setPower(v4);
+        backRight.setPower(v4);*/
         intakeRight.setPower(gamepad2.right_stick_y);
         intakeLeft.setPower(-gamepad2.right_stick_y);
        /* linearLift.setPower(gamepad2.right_stick_y * .65);
